649fe8fd4328d08a10b640edc5f23a81789bf733
tuposilwe <rudigermkondya@gmail.com>
2026-01-30 11:26:16 +0300

    Added Server Config

---
+++ my_site/run_server.py
@@ -0,0 +1,6 @@
+from waitress import serve
+from main.wsgi import application
+
+if __name__ == '__main__':
+    # Starts the server on port 8000
+    serve(application, host='0.0.0.0', port='8000')
