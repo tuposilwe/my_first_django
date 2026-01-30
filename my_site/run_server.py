81cebd59100487edc079895e0e85e250af7e19fc
tuposilwe <rudigermkondya@gmail.com>
2026-01-30 11:29:15 +0300

    Added threads

--- my_site/run_server.py
+++ my_site/run_server.py
@@ -3,4 +3,4 @@ from main.wsgi import application
 
 if __name__ == '__main__':
     # Starts the server on port 8000
-    serve(application, host='0.0.0.0', port='8000')
+    serve(application, host='0.0.0.0', port='8000',threads=4)
